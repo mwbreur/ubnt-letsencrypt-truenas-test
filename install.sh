d15d3dd6548ee3844f668e90bd5d476b929ec4ec
Michiel <michiel.w.breur@gmail.com>
2025-01-22 12:53:58 +0100

    Update install.sh
    trying to get this to work on truenas scale.

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ACMEHOME=/config/.acme.sh
-CA_BUNDLE=/config/ssl/cacert.pem
+ACMEHOME=/home/config/.acme.sh
+CA_BUNDLE=/home/config/ssl/cacert.pem
 
-mkdir -p ${ACMEHOME} /config/scripts /config/ssl
+mkdir -p ${ACMEHOME} /home/config/scripts /home/config/ssl
 curl -o ${ACMEHOME}/acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
-curl -o /config/scripts/renew.acme.sh https://raw.githubusercontent.com/j-c-m/ubnt-letsencrypt/master/renew.acme.sh
-chmod 755 ${ACMEHOME}/acme.sh /config/scripts/renew.acme.sh
+curl -o /home/config/scripts/renew.acme.sh https://raw.githubusercontent.com/j-c-m/ubnt-letsencrypt/master/renew.acme.sh
+chmod 755 ${ACMEHOME}/acme.sh /home/config/scripts/renew.acme.sh
 
 if [ -s ${CA_BUNDLE} ]; then
     curl https://curl.se/ca/cacert.pem -z ${CA_BUNDLE} -o ${CA_BUNDLE}
@@ -14,8 +14,8 @@ else
     curl -k https://curl.se/ca/cacert.pem -o ${CA_BUNDLE}
 fi
 
-if ! [ -f ${ACMEHOME}/account.conf ]; then
-    touch ${ACMEHOME}/account.conf
+if ! [ -f ${ACMEHOME}/home/account.conf ]; then
+    touch ${ACMEHOME}/home/account.conf
 fi
 
-sed -i "/^CA_BUNDLE='${CA_BUNDLE//\//\\/}'\$/h;\${x;/^\$/{s//CA_BUNDLE='${CA_BUNDLE//\//\\/}'/;H};x}" ${ACMEHOME}/account.conf
+sed -i "/^CA_BUNDLE='${CA_BUNDLE//\//\\/}'\$/h;\${x;/^\$/{s//CA_BUNDLE='${CA_BUNDLE//\//\\/}'/;H};x}" ${ACMEHOME}/home/account.conf
